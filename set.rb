1e38d95ca5d95df031134edcac67c7e6cf4ef9a0
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-14 20:01:16 -0800

    implement set

--- set.rb
+++ set.rb
@@ -1,16 +1,39 @@
-require_relative 'linked_list'
+require_relative 'array_list'
 
 class MySet
   attr_accessor :collection
+  attr_reader :size
 
   def initialize
-    @collection = LinkedList.new
+    @collection = Array.new
+    @size = 0
   end
 
   def add(element)
-    @collection.insert_first(element)
+    unless contains?(element)
+      @collection[hash_value(element)] = element
+      @size +=1
+    end
   end
 
   def remove(element)
+    if contains?(element)
+      @collection[hash_value(element)] = nil
+      @size -= 1
+    end
+  end
+
+  def iterate(&block)
+    @collection.reject(&:nil?).each do |value|
+      yield value
+    end
+  end
+
+  def contains?(element)
+    @collection.include?(element)
+  end
+
+  def hash_value(value)
+    value.to_sym.object_id % 100
   end
 end
--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -7,13 +7,61 @@ describe MySet do
     it 'should create a an instance of MySet' do
       expect(my_set).to be_a MySet
     end
-
-    it 'should instantiate with an empty collection 'do
-    end
   end
 
   describe '#add' do
     it 'should add an element to the collection' do
+      expect { my_set.add('value1') }.to change { my_set.size }.by 1
+    end
+
+    it 'should not add a new element if the element already exists' do
+      my_set.add('value1')
+      expect { my_set.add('value1') }.to_not change { my_set.size }
+    end
+  end
+
+  describe '#remove' do
+    before :each do
+      my_set.add('value1')
+      my_set.add('value2')
+    end
+
+    it 'should remove the element if the element is in the set' do
+      my_set.add('value1')
+      expect { my_set.remove('value1') }.to change { my_set.size }.by -1
+    end
+
+
+    it 'should not change the size of the set if the element does not exist' do
+      expect { my_set.remove('value3') }.to_not change { my_set.size }
+    end
+  end
+
+  describe '#contains?' do
+    before :each do
+      my_set.add('value1')
+      my_set.add('value2')
+    end
+
+    it 'should return true if the element is already in the collection' do
+      expect(my_set.contains?('value1')).to eq true
+      expect(my_set.contains?('value2')).to eq true
+    end
+
+    it 'should return false if the element is not in the collection' do
+      expect(my_set.contains?('value3')).to eq false
+      expect(my_set.contains?('value4')).to eq false
+    end
+  end
+
+  describe '#size' do
+    before :each do
+      my_set.add('value1')
+      my_set.add('value2')
+    end
+
+    it 'should return the correct size' do
+      expect(my_set.size).to eq 2
     end
   end
 end
